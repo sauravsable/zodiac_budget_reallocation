6d6bff6b115f957ab977ec761dcebf5a51ab673f
Sandeep Singh <sandeepsinghh143@gmail.com>
2025-07-16 11:07:28 +0530

    platform only zepto

--- src/components/PlatformSwitch.tsx
+++ src/components/PlatformSwitch.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 
 const PlatformSwitch = () => {
-  const [selected, setSelected] = useState("Blinkit");
+  const [selected, setSelected] = useState("Zepto");
 
   const platforms = [
     {
       label: "Blinkit",
       bgClass: "bg-yellow-600",
+      disabled: true,
     },
     {
       label: "Zepto",
       bgClass: "bg-purple-600",
+      disabled: false,
     },
     {
       label: "Instamart",
       bgClass: "bg-orange-600",
+      disabled: true,
     },
   ];
 
@@ -24,7 +27,7 @@ const PlatformSwitch = () => {
         {/* <span className="text-white text-base font-medium">Select Platform:</span> */}
 
         <div className="flex gap-2">
-          {platforms.map(({ label, bgClass }) => {
+          {platforms.map(({ label, bgClass, disabled }) => {
             const isSelected = selected === label;
 
             return (
@@ -32,13 +35,15 @@ const PlatformSwitch = () => {
                 key={label}
                 role="button"
                 tabIndex={0}
-                onClick={() => setSelected(label)}
+                onClick={() => !disabled && setSelected(label)}
                 onKeyPress={(e) => e.key === "Enter" && setSelected(label)}
-                className={`px-4 py-1.5 text-sm font-medium cursor-pointer border transition-all duration-200 rounded-md
-  ${isSelected
-                    ? `${bgClass} text-white border-transparent`
-                    : "bg-slate-800 text-white border border-white/20 hover:border-white/40 hover:bg-slate-700"
-                  }`}
+                className={`px-4 py-1.5 text-sm font-medium border transition-all duration-200 rounded-md
+  ${
+    isSelected
+      ? `${bgClass} text-white border-transparent`
+      : "bg-slate-800 text-white border border-white/20 hover:border-white/40 hover:bg-slate-700"
+  } ${!disabled ? "cursor-pointer" : "cursor-not-allowed"}
+`}
               >
                 {label}
               </div>
